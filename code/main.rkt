2b7a6055b7fac03a6f9fcfb1432618f491e57c08
l0w <orbit@192.168.11.112>
2026-02-21 21:08:42 +0000

    #2

--- code/main.rkt
+++ code/main.rkt
@@ -1,2 +1,20 @@
 #lang sicp
 
+(define (sum x y) (+ x y))
+(define (mul x y) (* x y))
+(define (div x y) (/ x y))
+(define (sub x y) (- x y))
+(define result 0)
+
+(define (square x) (mul x x))
+
+(define (proc x y z)
+  (cond
+    ((and (<= x y) (<= x z)) (sum (square y) (square z)))
+    ((and (<= y x) (<= y z)) (sum (square x) (square z)))
+    ((and (<= z x) (<= z y)) (sum (square x) (square y)))
+    ))
+
+(proc 10 200 3)
+
+ 
